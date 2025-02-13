312c7dbf76d9bd140fe4ae27d93cb43604a26b6d
Michaël A. <ahouandjinoubill@gmail.com>
2025-02-13 16:53:46 +0100

    Tasks Pagination.

--- src/contexts/routes.js
+++ src/contexts/routes.js
@@ -5,4 +5,24 @@ const router = Router();
 router.use("/tasks", require("./task"));
 router.use("/tags", require("./tag"));
 
+router.get("/limit", async (data, response) => {
+    const page = parseInt(data.query.page) || 1;
+    const pageSize = 10;
+    const skip = (page - 1) * pageSize;
+    try {
+        const tasks = await client.task.findMany({
+            skip: skip,
+            take: pageSize,
+            orderBy: { priority: 'asc' },
+            include: { tags: true }
+        });
+    response.status(200).json({ tasks: tasks, page: page });
+    } catch (error) {
+        response.status(500).json({
+            status: false,
+            error: "Error fetching tasks. Please try again later."
+        });
+    }
+});
+
 module.exports = router;
